79855a82d8af464a6c50efbc58c37d602049ba3a
Nils Rohe <nilsrohe@freenet.de>
2020-03-08 19:18:19 +0100

    refactor lists

--- db/queries.lists.js
+++ db/queries.lists.js
@@ -3,78 +3,78 @@ const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 module.exports = {
-    async createList(newList, req, callback) {
-        try {
-            const createdList = await List.create({
-                description: newList.description,
-                userId: newList.userId
-            });
-            const allLists = await List.findAll({
-                where: {
-                    [Op.or]: [
-                        { userId: createdList.userId },
-                        { '$collabs.userId$': createdList.userId }
-                    ]
-                },
-                include: [
-                    {
-                        model: Item,
-                        as: 'items'
-                    },
-                    {
-                        model: Collab,
-                        as: 'collabs',
-                        where: { userId: createdList.userId },
-                        required: false
-                    }
-                ]
-            });
-            callback(null, allLists);
-        } catch (err) {
-            callback(err);
-        }
-    },
-    async deleteList(data, req, callback) {
-        try {
-            let list = await List.findByPk(data.listId);
-            if (+list.userId === +data.userId) {
-                list.destroy();
-            } else {
-                throw 500;
-            }
-            const allLists = await List.findAll({
-                where: {
-                    [Op.or]: [
-                        { userId: data.userId },
-                        { '$collabs.userId$': data.userId }
-                    ]
-                },
-                include: [
-                    {
-                        model: Item,
-                        as: 'items'
-                    },
-                    {
-                        model: Collab,
-                        as: 'collabs',
-                        where: { userId: data.userId },
-                        required: false
-                    }
-                ]
-            });
-            if (allLists && allLists.length > 0) {
-                callback(null, allLists);
-            } else {
-                let newList = await List.create({
-                    description: 'New list',
-                    userId: data.userId
-                });
-                callback(null, [...allLists, newList]);
-            }
-        } catch (err) {
-            callback('error');
-        }
-    }
+    // async createList(newList, req, callback) {
+    //     try {
+    //         const createdList = await List.create({
+    //             description: newList.description,
+    //             userId: newList.userId
+    //         });
+    //         const allLists = await List.findAll({
+    //             where: {
+    //                 [Op.or]: [
+    //                     { userId: createdList.userId },
+    //                     { '$collabs.userId$': createdList.userId }
+    //                 ]
+    //             },
+    //             include: [
+    //                 {
+    //                     model: Item,
+    //                     as: 'items'
+    //                 },
+    //                 {
+    //                     model: Collab,
+    //                     as: 'collabs',
+    //                     where: { userId: createdList.userId },
+    //                     required: false
+    //                 }
+    //             ]
+    //         });
+    //         callback(null, allLists);
+    //     } catch (err) {
+    //         callback(err);
+    //     }
+    // },
+    // async deleteList(data, req, callback) {
+    //     try {
+    //         let list = await List.findByPk(data.listId);
+    //         if (+list.userId === +data.userId) {
+    //             list.destroy();
+    //         } else {
+    //             throw 500;
+    //         }
+    //         const allLists = await List.findAll({
+    //             where: {
+    //                 [Op.or]: [
+    //                     { userId: data.userId },
+    //                     { '$collabs.userId$': data.userId }
+    //                 ]
+    //             },
+    //             include: [
+    //                 {
+    //                     model: Item,
+    //                     as: 'items'
+    //                 },
+    //                 {
+    //                     model: Collab,
+    //                     as: 'collabs',
+    //                     where: { userId: data.userId },
+    //                     required: false
+    //                 }
+    //             ]
+    //         });
+    //         if (allLists && allLists.length > 0) {
+    //             callback(null, allLists);
+    //         } else {
+    //             let newList = await List.create({
+    //                 description: 'New list',
+    //                 userId: data.userId
+    //             });
+    //             callback(null, [...allLists, newList]);
+    //         }
+    //     } catch (err) {
+    //         callback('error');
+    //     }
+    // }
     // async getLists(userId, req, callback) {
     //     try {
     //         const lists = await List.findAll({
--- routes/lists.js
+++ routes/lists.js
@@ -5,6 +5,23 @@ const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 const { List, Collab, Item } = require('../db/models');
 
+// @route       POST api/lists
+// @desc        Create a new list
+// @access      Private
+
+router.post('/', auth, async (req, res) => {
+    try {
+        const createdList = await List.create({
+            description: newList.description,
+            userId: newList.userId
+        });
+        res.json({ createdList });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
 // @route       GET api/lists
 // @desc        Get all lists incl items
 // @access      Private
@@ -41,6 +58,54 @@ router.get('/', auth, async (req, res) => {
             });
             allLists = [...lists, newList];
             res.json({ allLists });
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
+
+// @route       GET api/lists
+// @desc        Get all lists incl items
+// @access      Private
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        const { listId } = req.body;
+        let list = await List.findByPk(listId);
+        if (+list.userId === req.user.id) {
+            list.destroy();
+        } else {
+            res.status(401).send('Not allowed!');
+        }
+        const allLists = await List.findAll({
+            where: {
+                [Op.or]: [
+                    { userId: req.user.id },
+                    { '$collabs.userId$': req.user.id }
+                ]
+            },
+            include: [
+                {
+                    model: Item,
+                    as: 'items'
+                },
+                {
+                    model: Collab,
+                    as: 'collabs',
+                    where: { userId: req.user.id },
+                    required: false
+                }
+            ]
+        });
+        if (allLists && allLists.length > 0) {
+            res.json({ allLists });
+        } else {
+            let newList = await List.create({
+                description: 'New list',
+                userId: req.user.id
+            });
+            res.json({ newList });
         }
     } catch (err) {
         console.error(err);
